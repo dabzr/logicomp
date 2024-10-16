e705dd0a404fbaf4ba7ff8d2d2480236e0a8f457
dabzr <dylanab@pm.me>
2024-10-16 05:48:56 -0300

    Simplificando o código das funções

--- functions.py
+++ functions.py
@@ -2,20 +2,19 @@
 do some computation on its syntactic structure. """
 
 
-from formula import *
+from formula import Formula, Atom, Not, Implies, And, Or
 
 
-def length(formula: Formula):
+def length(formula: Formula) -> int:
     """Determines the length of a formula in propositional logic."""
-    if isinstance(formula, Atom):
-        return 1
     if isinstance(formula, Not):
         return length(formula.inner) + 1
-    if isinstance(formula, Implies) or isinstance(formula, And) or isinstance(formula, Or):
+    if isinstance(formula, (Implies, And, Or)):
         return length(formula.left) + length(formula.right) + 1
+    return 1
 
 
-def subformulas(formula: Formula):
+def subformulas(formula: Formula) -> set:
     """Returns the set of all subformulas of a formula.
 
     For example, observe the piece of code below.
@@ -27,15 +26,13 @@ def subformulas(formula: Formula):
     This piece of code prints p, s, (p v s), (p → (p v s))
     (Note that there is no repetition of p)
     """
-
-    if isinstance(formula, Atom):
-        return {formula}
     if isinstance(formula, Not):
         return {formula}.union(subformulas(formula.inner))
-    if isinstance(formula, Implies) or isinstance(formula, And) or isinstance(formula, Or):
+    if isinstance(formula, (Implies, And, Or)):
         sub1 = subformulas(formula.left)
         sub2 = subformulas(formula.right)
         return {formula}.union(sub1).union(sub2)
+    return {formula}
 
 #  we have shown in class that, for all formula A, len(subformulas(A)) <= length(A).
 
@@ -57,65 +54,52 @@ def atoms(formula: Formula) -> set:
         atoms_set.add(formula)
     if isinstance(formula, Not):
         atoms_set.update(atoms(formula.inner))
-    if isinstance(formula, Implies) or isinstance(formula, And) or isinstance(formula, Or):
+    if isinstance(formula, (Implies, And, Or)):
         atoms_set.update(atoms(formula.left))
         atoms_set.update(atoms(formula.right))
     return atoms_set
 
 
-def number_of_atoms(formula: Formula):
+def number_of_atoms(formula: Formula) -> int:
     """Returns the number of atoms occurring in a formula.
     For instance,
     number_of_atoms(Implies(Atom('q'), And(Atom('p'), Atom('q'))))
 
     must return 3 (Observe that this function counts the repetitions of atoms)
     """
-    if isinstance(formula, Atom):
-        return 1 
     if isinstance(formula, Not):
         return number_of_atoms(formula.inner)
-    if isinstance(formula, Implies) or isinstance(formula, And) or isinstance(formula, Or):
+    if isinstance(formula, (Implies, And, Or)):
         return number_of_atoms(formula.left) + number_of_atoms(formula.right)
-
-def number_of_connectives(formula: Formula):
+    return 1
+def number_of_connectives(formula: Formula) -> int:
     """Returns the number of connectives occurring in a formula."""
-    if isinstance(formula, Atom):
-        return 0
     if isinstance(formula, Not):
         return number_of_connectives(formula.inner) + 1
-    if isinstance(formula, Implies) or isinstance(formula, And) or isinstance(formula, Or):
+    if isinstance(formula, (Implies, And, Or)):
         return number_of_connectives(formula.left) + number_of_connectives(formula.right) + 1
+    return 0
 
-
-def is_literal(formula: Formula):
+def is_literal(formula: Formula) -> bool:
     """Returns True if formula is a literal. It returns False, otherwise"""
-    if isinstance(formula, Atom):
-        return True
-    if isinstance(formula, Not) and isinstance(formula.inner, Atom):
-        return True
-    return False
+    return isinstance(formula, Atom) or (isinstance(formula, Not) and isinstance(formula.inner, Atom))
 
-def substitution(formula: Formula, old_subformula: Formula, new_subformula: Formula):
+def substitution(formula: Formula, old_subformula: Formula, new_subformula: Formula) -> Formula:
     """Returns a new formula obtained by replacing all occurrences
     of old_subformula in the input formula by new_subformula."""
+    if formula == old_subformula:
+        return new_subformula
     if isinstance(formula, Not):
-        if formula.inner == old_subformula:
-            formula.inner = new_subformula
-        substitution(formula.inner, old_subformula, new_subformula)
-    if isinstance(formula, Implies) or isinstance(formula, And) or isinstance(formula, Or):
-        if formula.left == old_subformula:
-            formula.left = new_subformula
-        if formula.right == old_subformula:
-            formula.right = new_subformula
-        substitution(formula.left, old_subformula, new_subformula)
-        substitution(formula.right, old_subformula, new_subformula)
+        formula.inner = substitution(formula.inner, old_subformula, new_subformula)
+    if isinstance(formula, (Implies, And, Or)):
+        formula.left = substitution(formula.left, old_subformula, new_subformula)
+        formula.right = substitution(formula.right, old_subformula, new_subformula)
+    return formula
 
-def is_clause(formula: Formula):
+def is_clause(formula: Formula) -> bool:
     """Returns True if formula is a clause. It returns False, otherwise"""
     if isinstance(formula, Or):
-        condL = is_literal(formula.left) or is_clause(formula.left)
-        condR = is_literal(formula.right) or is_clause(formula.right)
-        return condL and condR
+        return is_clause(formula.left) and is_clause(formula.right)
     return is_literal(formula)
 
 
