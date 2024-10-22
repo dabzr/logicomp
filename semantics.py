57f96229177531857c7d708159cc55184b3d44d8
dabzr <dylanab@pm.me>
2024-10-22 06:23:00 -0300

    Adicionando função de interpretação parcial

--- semantics.py
+++ semantics.py
@@ -5,7 +5,7 @@ from formula import *
 from functions import atoms
 
 
-def truth_value(formula, interpretation):
+def truth_value(formula: Formula, interpretation: dict):
     """Determines the truth value of a formula in an interpretation (valuation).
     An interpretation may be defined as dictionary. For example, {'p': True, 'q': False}.
     """
@@ -20,7 +20,25 @@ def truth_value(formula, interpretation):
     if isinstance(formula, Implies):
         return not truth_value(formula.left, interpretation) or truth_value(formula.right, interpretation)
 
-
+def partial_truth_value(formula: Formula, partial_interpretation: dict):
+    if isinstance(formula, Atom):
+        return partial_interpretation.get(formula)
+    if isinstance(formula, Not):
+        if partial_truth_value(formula.inner, partial_interpretation) != None:
+            return not partial_truth_value(formula.inner, partial_interpretation)
+    if isinstance(formula, Or):
+        pl = partial_truth_value(formula.left, partial_interpretation)
+        pr = partial_truth_value(formula.right, partial_interpretation)
+        return pl or pr
+    if isinstance(formula, And):
+        pl = partial_truth_value(formula.left, partial_interpretation)
+        pr = partial_truth_value(formula.right, partial_interpretation)
+        return pl and pr
+    if isinstance(formula, Implies):
+        pl = partial_truth_value(formula.left, partial_interpretation)
+        pr = partial_truth_value(formula.right, partial_interpretation)
+        return not pl or pr
+    return None
 
 def is_logical_consequence(premises, conclusion):  # function TT-Entails? in the book AIMA.
     """Returns True if the conclusion is a logical consequence of the set of premises. Otherwise, it returns False."""
