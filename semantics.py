0e047d1191dfdbf62fa5da39b63aba8ddeaffa7a
dabzr <dylanab@pm.me>
2024-11-07 20:35:06 -0300

    deixando código mais aesthetic e muito menos entendível

--- semantics.py
+++ semantics.py
@@ -3,14 +3,13 @@
 from typing import List
 from formula import Formula, Not, Or, And, Implies, Atom
 from functions import atoms
+from functools import reduce
+from itertools import product
 
-
-def truth_value(formula: Formula, interpretation: dict):
+def truth_value(formula: Formula, interpretation: dict) -> bool:
     """Determines the truth value of a formula in an interpretation (valuation).
     An interpretation may be defined as dictionary. For example, {'p': True, 'q': False}.
     """
-    if isinstance(formula, Atom):
-        return interpretation[formula]
     if isinstance(formula, Not):
         return not truth_value(formula.inner, interpretation)
     if isinstance(formula, Or):
@@ -19,50 +18,35 @@ def truth_value(formula: Formula, interpretation: dict):
         return truth_value(formula.left, interpretation) and truth_value(formula.right, interpretation)
     if isinstance(formula, Implies):
         return not truth_value(formula.left, interpretation) or truth_value(formula.right, interpretation)
+    return interpretation[formula] # Caso seja Atom
 
-def partial_truth_value(formula: Formula, partial_interpretation: dict):
+def partial_truth_value(formula: Formula, interp: dict):
     if isinstance(formula, Atom):
-        return partial_interpretation.get(formula)
+        return interp.get(formula)
     if isinstance(formula, Not):
-        if partial_truth_value(formula.inner, partial_interpretation) is not None:
-            return not partial_truth_value(formula.inner, partial_interpretation)
+        if partial_truth_value(formula.inner, interp) is not None:
+            return not partial_truth_value(formula.inner, interp)
     if isinstance(formula, Or):
-        pl = partial_truth_value(formula.left, partial_interpretation)
-        pr = partial_truth_value(formula.right, partial_interpretation)
-        return pl or pr
+        return partial_truth_value(formula.left, interp) or partial_truth_value(formula.right, interp)
     if isinstance(formula, And):
-        pl = partial_truth_value(formula.left, partial_interpretation)
-        pr = partial_truth_value(formula.right, partial_interpretation)
-        return pl and pr
+        return partial_truth_value(formula.left, interp) and partial_truth_value(formula.right, interp)
     if isinstance(formula, Implies):
-        pl = partial_truth_value(formula.left, partial_interpretation)
-        pr = partial_truth_value(formula.right, partial_interpretation)
-        return not pl or pr
+        return not partial_truth_value(formula.left, interp) or partial_truth_value(formula.right, interp)
     return None
 
 def create_truth_table(formula: Formula):
     atoms_list = list(atoms(formula))
+    truth_combinations = product([False, True], repeat=len(atoms_list))
     truth_table = []
-    num_atoms = len(atoms_list)
-    size = 2 ** num_atoms
-    for i in range(size):
-        logic_dict = {}
-        for j, atom in enumerate(atoms_list):
-            logic_dict[atom] = (i >> (num_atoms - j - 1)) & 1 == 1
-        logic_dict[formula] = truth_value(formula, logic_dict)
-        truth_table.append(logic_dict)
+    for combination in truth_combinations:
+        row = dict(zip(atoms_list, combination)) 
+        row[formula] = truth_value(formula, row)
+        truth_table.append(row)
     return truth_table
 
 def is_logical_consequence(premises: List[Formula], conclusion: Formula):  # function TT-Entails? in the book AIMA.
     """Returns True if the conclusion is a logical consequence of the set of premises. Otherwise, it returns False."""
-    if not premises:
-       return is_valid(conclusion)
-    if len(premises) == 1:
-        return is_valid(Implies(premises[0], conclusion))
-    formula = And(premises[0], premises[1])
-    for i in range(2, len(premises)):
-        formula = And(formula, premises[i])
-    return is_valid(Implies(formula, conclusion))
+    return is_valid(conclusion) if not premises else is_valid(Implies(reduce(lambda acc, pre: And(acc, pre), premises), conclusion))
 
 def is_logical_equivalence(formula1, formula2): 
     """Checks whether formula1 and formula2 are logically equivalent."""
@@ -71,7 +55,6 @@ def is_logical_equivalence(formula1, formula2):
 def is_valid(formula):
     """Returns True if formula is a logically valid (tautology). Otherwise, it returns False"""
     return all([i[formula] is True for i in create_truth_table(formula)])
-
 
 def satisfiability_brute_force(formula):
     """Checks whether formula is satisfiable.
